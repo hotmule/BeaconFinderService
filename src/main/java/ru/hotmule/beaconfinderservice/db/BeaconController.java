efc2c46b680e4bdffb4f3e4f08c80028e3635dff
hotmule <t0t0t0t0t0>
2019-10-30 18:06:11 +0300

    Database changelog update

--- src/main/java/ru/hotmule/beaconfinderservice/db/BeaconController.java
+++ src/main/java/ru/hotmule/beaconfinderservice/db/BeaconController.java
@@ -1,4 +1,4 @@
-package ru.hotmule.beaconfinderservice;
+package ru.hotmule.beaconfinderservice.db;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
--- src/main/java/ru/hotmule/beaconfinderservice/db/BeaconRepository.java
+++ src/main/java/ru/hotmule/beaconfinderservice/db/BeaconRepository.java
@@ -1,4 +1,4 @@
-package ru.hotmule.beaconfinderservice;
+package ru.hotmule.beaconfinderservice.db;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
