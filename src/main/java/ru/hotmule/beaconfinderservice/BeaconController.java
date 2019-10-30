dbc7715ed4aa62a1d5a06fbb8bd515a2364209fb
hotmule <t0t0t0t0t0>
2019-10-30 17:26:51 +0300

    Database added

--- src/main/java/ru/hotmule/beaconfinderservice/BeaconController.java
+++ src/main/java/ru/hotmule/beaconfinderservice/BeaconController.java
@@ -1,4 +1,4 @@
-package ru.hotmule.beaconfinderservice.db;
+package ru.hotmule.beaconfinderservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
