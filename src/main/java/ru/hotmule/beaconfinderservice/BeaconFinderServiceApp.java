ec4f7aa1e56ac46dd4c1bc35e2e0e218260216d5
hotmule <t0t0t0t0t0>
2019-10-30 15:04:47 +0300

    Initial commit

---
+++ src/main/java/ru/hotmule/beaconfinderservice/BeaconFinderServiceApp.java
@@ -0,0 +1,21 @@
+package ru.hotmule.beaconfinderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
+
+@Controller
+@SpringBootApplication
+public class BeaconFinderServiceApp {
+
+  @RequestMapping("/")
+  @ResponseBody
+  String home() {
+    return "BeaconFinderService is running...";
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(BeaconFinderServiceApp.class, args);
+  }
+}
